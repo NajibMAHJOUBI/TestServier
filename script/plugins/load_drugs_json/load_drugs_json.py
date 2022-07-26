9253f836c9b727747d806470741347a7940fcac7
mahjoubi <najib.mahjoubi@gmail.com>
2022-07-27 00:46:55 +0200

    reprise de la sauvegarde en json

--- script/plugins/load_drugs_json/load_drugs_json.py
+++ script/plugins/load_drugs_json/load_drugs_json.py
@@ -1,25 +1,9 @@
-import ast
-import json
-from functools import reduce
 from typing import TYPE_CHECKING
 
 import pandas as pd
 
 if TYPE_CHECKING:
     from airflow.models.taskinstance import TaskInstance
-
-
-def merge_dict(dict1, dict2):
-    merged = dict()
-    for journal in set(list(dict1.keys()) + list(dict2.keys())):
-        merged[journal] = dict()
-        for date in set(list(dict1.get(journal, {}).keys()) + list(dict2.get(journal, {}).keys())):
-            merged[journal][date] = dict1.get(journal, {}).get(date, []) + dict2.get(journal, {}).get(date, [])
-    return merged
-
-
-def reduce_dict(series):
-    return reduce(lambda dict1, dict2: merge_dict(dict1, dict2), series)
 
 
 def load_drugs_json(path_save: str, ti: 'TaskInstance') -> None:
@@ -38,14 +22,18 @@ def load_drugs_json(path_save: str, ti: 'TaskInstance') -> None:
     path_transform = ti.xcom_pull(task_ids='transform_drugs_publication', key='transform')
     df = pd.read_csv(path_transform, dtype=str, usecols=['title', 'date', 'journal', 'drug'])
 
-    df['values'] = df.apply(lambda row: {row['journal']: {str(row['date']): [row['title']]}}, axis=1)
+    df['publication'] = df.apply(lambda row: {'Date': row['date'], 'Titre': row['title']}, axis=1)
 
-    df = df.groupby('drug').agg({'values': reduce_dict}).reset_index()
+    df.drop(columns=['title', 'date'], inplace=True)
 
-    # df['values'] = df['values'].map(lambda value: ast.literal_eval(value))
+    df = df.groupby(['drug', 'journal']).agg(list).reset_index()
 
-    json_data = df.set_index('drug').to_dict()['values']
+    df['citation'] = df.apply(lambda row: {'Journal': row['journal'], 'Publication': row['publication']}, axis=1)
 
-    with open(path_save, "w") as write_file:
-        json.dump(json_data, write_file, indent=4)
-    write_file.close()
+    df.drop(columns=['journal', 'publication'], inplace=True)
+
+    df.rename(columns={'drug': 'Medicament'}, inplace=True)
+
+    df = df.groupby(['Medicament']).agg(list).reset_index()
+
+    df.to_json(path_save, orient='records', indent=4)
